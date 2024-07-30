f8607da1d09d87d7075d79c1c4aaa51e8a984906
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2024-07-30 10:53:55 +0530

    Handling Loading Animations Done

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ const chatList = document.querySelector(".chat-list");
 let userMessage = null;
 
 // Create a new message element and return it
-const createMessageElement = (content, className) => {
+const createMessageElement = (content, ...classes) => {
 	const div = document.createElement("div");
-	div.classList.add("message", className);
+	div.classList.add("message", ...classes);
 	div.innerHTML = content;
 	return div;
+};
+
+// Show a loading animation while waiting for the API response
+const showLoadingAnimation = () => {
+	const html = `<div class="message-content">
+					<img src="img/gemini.svg" alt="Gemini Image" class="avatar" />
+					<p class="text"></p>
+                    <div class="loading-indicator">
+                        <div class="loading-bar"></div>
+                        <div class="loading-bar"></div>
+                        <div class="loading-bar"></div>
+                    </div>
+				</div>
+				<span class="icon material-symbols-rounded">content_copy</span>`;
+
+	const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
+	chatList.appendChild(incomingMessageDiv);
 };
 
 // Handle sending outgoing messages
@@ -27,7 +44,9 @@ const handleOutgoingChat = () => {
 	chatList.appendChild(outgoingMessageDiv);
 
 	// Clear the input field after sending the message
-	typingForm.querySelector(".typing-input").value = '';
+	// typingForm.querySelector(".typing-input").value = '';
+	typingForm.reset(); // Clear Input Field
+	setTimeout(showLoadingAnimation, 500); // Show loading animation after a delay
 };
 
 // Prevent default from submission and handle outgoing chat
