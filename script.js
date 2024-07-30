162748ca57788e9b4fbe05aff4fb33e9f34a2377
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2024-07-30 11:20:02 +0530

    Checkpoint: Getting API Response To Console

--- script.js
+++ script.js
@@ -3,12 +3,42 @@ const chatList = document.querySelector(".chat-list");
 
 let userMessage = null;
 
+// API Configuration
+const API_KEY = "ENTER_YOUR_API"; // Enter your own API Key
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+
 // Create a new message element and return it
 const createMessageElement = (content, ...classes) => {
 	const div = document.createElement("div");
 	div.classList.add("message", ...classes);
 	div.innerHTML = content;
 	return div;
+};
+
+// Fetch response from the API based on user message
+const generateAPIResponse = async () => {
+	// Send a POST request to the API with user's message
+	try {
+		const response = await fetch(API_URL, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				contents: [
+					{
+						role: "user",
+						parts: [{ text: userMessage }],
+					},
+				],
+			}),
+		});
+
+		const data = await response.json();
+
+		const apiResponse = data?.candidates[0].content.parts[0].text;
+		console.log(apiResponse);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // Show a loading animation while waiting for the API response
@@ -26,6 +56,8 @@ const showLoadingAnimation = () => {
 
 	const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
 	chatList.appendChild(incomingMessageDiv);
+
+	generateAPIResponse();
 };
 
 // Handle sending outgoing messages
